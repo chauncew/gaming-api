81b102e73c40219f5d23da254af44a80f4246f90
Chaunce W <chauncew1137@gmail.com>
2022-12-18 08:40:20 -0600

    Adding styles and functionality to Search section

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const gamingInfo = document.querySelector('.gaming-info');
+const highestRated = document.querySelector('.highest-rated');
 const input = document.querySelector('.input-box')
 const submit = document.querySelector('.submit')
+const searchCards = document.querySelector('.search-cards')
 
-// const url = "https://api.rawg.io/api/games?key=d34c595a3f0b48eca8d4022bde535aba"
+const url = "https://api.rawg.io/api/games?key=d34c595a3f0b48eca8d4022bde535aba"
 
-
+//Fetching the search data
 submit.addEventListener('click', (e) => {
     e.preventDefault()
     const getAPI = async () => {
@@ -13,9 +14,9 @@ submit.addEventListener('click', (e) => {
         try {
         const api = await fetch(url2)
         const data = await api.json()
-        // games(data)
-        console.log(data)
+        // console.log(data)
         input.value = ""
+            gameSearch(data)
         } catch(error) {
             console.log(`Failed to fetch data ${error}`)
         }
@@ -23,69 +24,111 @@ submit.addEventListener('click', (e) => {
     getAPI()
 })
 
-// const games = (data) => {
+const gameSearch = (data) => {
+    data.results.forEach((search) => {
+        // const gameData = document.createElement('div')
+        console.log(search)
+        // searchCards.prepend(gameData)
+        searchCards.innerHTML += `
+        <div class="search-info">
+            <h1 class="search-name">${search.name}</h1>
+            <h3 class="search-date">Released: ${search.released}</h3>
+            <img class="search-image" src=${search.background_image}>
+        </div>
+        `
+    })
+}
+
+//Creating dates to fetch games
+const fetchCurrentMonth = () => {
+    const month = new Date().getMonth() + 1
+    if(month < 10) {
+        return `0{month}`
+    } else {
+        return month
+    }
+}
+fetchCurrentMonth()
+
+const fetchCurrentDay = () => {
+    const day = new Date().getDate()
+    if(day < 10) {
+        return `0${day}`
+    } else {
+        return day
+    }
+}
+fetchCurrentDay()
+
+const fetchCurrentYear = () => {
+    const year = new Date().getFullYear()
+}
+fetchCurrentYear()
+
+const currentYear = new Date().getFullYear()
+const currentMonth = fetchCurrentMonth()
+const currentDay = fetchCurrentDay()
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`
+
+
+//Fetching highest rated games
+//     const getApi = async () => {
+//         const url = `https://rawg.io/api/games?key=d34c595a3f0b48eca8d4022bde535aba&dates=${lastYear},${currentDate}&ordering=-rating`
+//         try {
+//         const api = await fetch(url)
+//         const data = await api.json()
+//         highestRatedGames(data)
+//         console.log(data)
+//         input.value = ""
+//         } catch(error) {
+//             console.log(`Failed to fetch data ${error}`)
+//         }
+//     }
+//     getApi()
+
+
+// const highestRatedGames = (data) => {
 //     data.results.forEach((info) => {
 //         // console.log(info)
-//         gamingInfo.innerHTML += `
+//         highestRated.innerHTML += `
+//         <div class="popular-games">
 //             <img class="gaming-image" src=${info.background_image}>
 //             <h3>${info.name}</h3>
+//         </div>
 //         `
 //     })
 // }
 
 
-
-
-//Fetching game dates
-// const fetchCurrentMonth = () => {
-//     const month = new Date().getMonth() + 1
-//     // console.log(month)
-//     if(month < 10) {
-//         return 
-//         `0${month}`
-//     }else {
-//         return month;
+//Fetching games coming soon
+//     const getApi2 = async () => {
+//         const url = `https://rawg.io/api/games?key=d34c595a3f0b48eca8d4022bde535aba&dates=${currentDate},${nextYear}&ordering=-rating`
+//         try {
+//         const api = await fetch(url)
+//         const data = await api.json()
+//         comingSoon(data)
+//         console.log(data)
+//         input.value = ""
+//         } catch(error) {
+//             console.log(`Failed to fetch data ${error}`)
+//         }
 //     }
+//     getApi2()
+
+
+// const comingSoon = (data) => {
+//     data.results.forEach((info) => {
+//         // console.log(info)
+//         highestRated.innerHTML += `
+//         <div class="popular-games">
+//             <img class="gaming-image" src=${info.background_image}>
+//             <h3>${info.name}</h3>
+//         </div>
+//         `
+//     })
 // }
-// fetchCurrentMonth()
 
-
-// const fetchCurrentDay = () => {
-//     const day = new Date().getDate()
-//     // console.log(day)
-//     if(day < 10) {
-//         return 
-//         `0${day}`
-//     }else {
-//         return day;
-//     }
-// }
-// fetchCurrentDay()
-
-//Combining current month, day, and year
-// const currentYear = new Date().getFullYear()
-// const currentMonth = fetchCurrentMonth()
-// const currentDay = fetchCurrentDay()
-// const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
-// console.log(currentDate)
-
-//Last Year Dates
-// const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`
-
-//Next Year Dates
-// const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`
-
-//Popular games dates
-// const popular_games =`${url}games?dates${lastYear},${currentDate}&ordering=-rating&page_size=10`;
-// console.log(popular_games)
-
-
-
-//Fetching the API for upcoming games
-
-
-
-
-//Fetching the API for new games
 
 //NASA API KEY: bgVXNGhNo3viRxKKc3SdD9LhDvPmJgYObDmZT7gQ
