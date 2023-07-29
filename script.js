84022ee57459721098603458eb1e05b83670fe71
Chaunce W <chauncew1137@gmail.com>
2023-07-29 09:58:10 -0500

    Puttinng finishing touches

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-const highestRated = document.querySelector('.highest-rated')
-const input = document.querySelector('.input-box')
-const submit = document.querySelector('.submit')
-const searchCards = document.querySelector('.search-cards')
-const scroll = document.querySelector('.scroll')
-const smallImages = document.querySelector('.small-images')
-const popupCards = document.querySelector('.popup-cards')
-const vale = document.querySelector('.vale')
-const body = document.querySelector('body')
-const mainHeading = document.querySelectorAll('.main-heading h1')
-const searchInfo = document.querySelectorAll('.search-info')
-const searchSection = document.querySelector('.search-section')
+const highestRated = document.querySelector('.highest-rated');
+const input = document.querySelector('.input-box');
+const submit = document.querySelector('.submit');
+const searchCards = document.querySelector('.search-cards');
+const scroll = document.querySelector('.scroll');
+const smallImages = document.querySelector('.small-images');
+const popupCards = document.querySelector('.popup-cards');
+const vale = document.querySelector('.vale');
+const body = document.querySelector('body');
+const mainHeading = document.querySelectorAll('.main-heading h1');
+const searchInfo = document.querySelectorAll('.search-info');
+const searchSection = document.querySelector('.search-section');
 
 
 const url = "https://api.rawg.io/api/games?key=d34c595a3f0b48eca8d4022bde535aba"
 
 //Fetching the search data
 submit.addEventListener('click', (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const getAPI = async () => {
         let value = input.value;
         const url2 = `https://api.rawg.io/api/games?key=d34c595a3f0b48eca8d4022bde535aba&search=${value}`
@@ -30,20 +30,19 @@ submit.addEventListener('click', (e) => {
             gameSearch(data)
         } catch(error) {
             console.log(`Failed to fetch data ${error}`)
-        }
-    }
-    getAPI()
+        };
+    };
+    getAPI();
     searchCards.innerHTML = ""
     // searchCards.style.display = "none"
-})
+});
 
 const gameSearch = (data) => {
     data.results.forEach((search) => {
         // console.log(search)
         const gameData = document.createElement('div')
-        //gameData Load Animation
         gsap.from(gameData, {y: -1000, x: -1000,  duration: 1.5, rotateY: 1080})
-        searchCards.append(gameData)
+        searchCards.append(gameData);
         gameData.innerHTML += `
         <div class="search-info">
             <h1 class="search-name">${search.name}</h1>
@@ -55,7 +54,7 @@ const gameSearch = (data) => {
         gameData.addEventListener('click', () => {
             vale.style.display = 'block'
             vale.style.transition = "all 0.5s"
-            popupCards.classList.add('cards')
+            popupCards.classList.add('cards');
             popupCards.innerHTML += `
             <h1 class="search-name">${search.name}</h1>
             <h3 class="rating">Rating: ${search.rating}</h3>
@@ -67,14 +66,14 @@ const gameSearch = (data) => {
                         <h3>Type:  ${genre.name}</h3>
                     </div>
                 `
-            })
+            });
             // console.log(gameData)
             search.platforms.map((gameData) => {
                 // console.log(gameData.platform.name)
                 popupCards.innerHTML += `
                     <h3 class="platforms">${gameData.platform.name}</h3>
                 `  
-            })
+            });
             popupCards.innerHTML += `
             <h1 class="close-button">X</h1>
             <img class="search-image" src=${search.background_image}>
@@ -87,21 +86,21 @@ const gameSearch = (data) => {
                     </div>
                 `
                 window.scrollTo({top: 0})
-            })
-        })
+            });
+        });
         popupCards.addEventListener('click', () => {
-            popupCards.classList.remove('cards') 
-            popupCards.innerHTML  = ""
+            popupCards.classList.remove('cards') ;
+            popupCards.innerHTML  = "";
             smallImages.classList.remove('smallImagesActive')
-            smallImages.innerHTML = ""
-            vale.style.display = "none"
-        })
+            smallImages.innerHTML = "";
+            vale.style.display = "none";
+        });
 
-    })
-}
+    });
+};
 
 //Animations
-gsap.registerPlugin()
+gsap.registerPlugin();
 //Heading Animation
 const animation = () => {
     const reveal = gsap.timeline()
@@ -113,62 +112,17 @@ const animation = () => {
         ease: 'bounce.out',
         duration: 1,
         stagger: 0.9,
-      })
-  }
+      });
+  };
 
   window.addEventListener('load', () => {
     setTimeout(() => {
         animation()
     }, 1000)
-  })
+  });
 
   //Search Box Animations
   const animation2 = () => {
-    gsap.from(searchSection, {autoAlpha: 0, y: 300, opacity: 0, duration: 1.5, delay: 4})
-  }
-  animation2()
-
-//Card Animation
-// const animation2 = gsap.timeline()
-// animation2
-// .fromTo(searchCards, {autoAlpha: 0, x: -2000, opacity: 0, scale: 7}, {x: 0, opacity: 1, scale: 1, duration: 0.7, stagger: 0.3})
-
-
-
-
-//Creating dates to fetch games
-// const fetchCurrentMonth = () => {
-//     const month = new Date().getMonth() + 1
-//     if(month < 10) {
-//         return `0{month}`
-//     } else {
-//         return month
-//     }
-// }
-// fetchCurrentMonth()
-
-// const fetchCurrentDay = () => {
-//     const day = new Date().getDate()
-//     if(day < 10) {
-//         return `0${day}`
-//     } else {
-//         return day
-//     }
-// }
-// fetchCurrentDay()
-
-// const fetchCurrentYear = () => {
-//     const year = new Date().getFullYear()
-// }
-// fetchCurrentYear()
-
-// const currentYear = new Date().getFullYear()
-// const currentMonth = fetchCurrentMonth()
-// const currentDay = fetchCurrentDay()
-// const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
-// const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`
-// const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`
-
-
-
-//NASA API KEY: bgVXNGhNo3viRxKKc3SdD9LhDvPmJgYObDmZT7gQ
+    gsap.from(searchSection, {autoAlpha: 0, y: 300, opacity: 0, duration: 1.5, delay: 4});
+  };
+  animation2();
